6642567561a408d8782f1c6bb39a272b561c7139
Do Hyung Kwon <thedavekwon@gmail.com>
2019-10-03 22:24:01 -0400

    hw2

--- hw2/recursive_file_lister.c
+++ hw2/recursive_file_lister.c
@@ -28,11 +28,11 @@ __uid_t curUid;
 gid_t *curGroups;
 struct passwd *curUserp;
 
-bool check_uid_or_user(char *curUser);
+bool check_uid_or_name(char *curUser);
 bool check_user(struct stat *statbuf);
 bool check_mtime(__time_t mtime);
 char *path_join(char *curPath, char *childPath);
-void recursive_travese(char *curPath);
+void recursive_traverse(char *curPath);
 void parse_info(struct dirent *direntp, struct stat *statbuf,
                 struct passwd *pwd, struct group *grp, char tmp[],
                 bool userNotFound, bool groupNotFound);
@@ -73,7 +73,7 @@ int main(int argc, char *argv[]) {
     case 'u':
       userFlag = true;
       curUser = optarg;
-      if (check_uid_or_user(optarg)) {
+      if (check_uid_or_name(optarg)) {
         curUserp = getpwuid(atoi(optarg));
         if (curUserp == NULL) {
           curUserNotFound = true;
@@ -81,16 +81,14 @@ int main(int argc, char *argv[]) {
         }
       } else {
         curUserp = getpwnam(optarg);
-        if (curUserp == NULL) {
+        if (curUserp == NULL)
           curUserNameNotFound = true;
-          // fprintf(stderr, "No user named %s found\n", curUser);
-          // exit(EXIT_FAILURE);
-        }
       }
       if (curUserp) {
         getgrouplist(curUser, curUserp->pw_gid, curGroups, &nGroups);
         curGroups = malloc(nGroups * sizeof(gid_t));
-        curGroupsCnt = getgrouplist(curUser, curUserp->pw_gid, curGroups, &nGroups);
+        curGroupsCnt =
+            getgrouplist(curUser, curUserp->pw_gid, curGroups, &nGroups);
       }
       break;
     default:
@@ -115,17 +113,19 @@ int main(int argc, char *argv[]) {
   }
 
   memcpy(startPath, curPath, strlen(curPath));
-  recursive_travese(curPath);
+  recursive_traverse(curPath);
+  free(curGroups);
   return 0;
 }
 
 char *path_join(char *curPath, char *childPath) {
-  if (strcmp(curPath, startPath) == 0 && strcmp(childPath, ".") == 0) return curPath;
+  if (strcmp(curPath, startPath) == 0 && strcmp(childPath, ".") == 0)
+    return curPath;
   return strcat(strcat(curPath, SEP), childPath);
 }
 
-void recursive_travese(char *curPath) {
-  bool userNotFound, groupNotFound;
+void recursive_traverse(char *curPath) {
+  bool userNotFound, groupNotFound, filtered;
   char tmp[4096];
   DIR *dirp;
   struct dirent *direntp;
@@ -135,13 +135,13 @@ void recursive_travese(char *curPath) {
 
   dirp = opendir(curPath);
   if (dirp == NULL) {
-    fprintf(stderr, "Can't open directory %s: %s\n", tmp, strerror(errno));
+    fprintf(stderr, "Can't open directory %s: %s\n", curPath, strerror(errno));
     return;
   }
   while ((direntp = readdir(dirp))) {
-    userNotFound = false;
-    groupNotFound = false;
-    if (direntp == NULL) break;
+    userNotFound = false, groupNotFound = false, filtered = false;
+    if (direntp == NULL)
+      break;
     if (strcmp(direntp->d_name, "..") == 0 ||
         (strcmp(direntp->d_name, ".") == 0 && strcmp(curPath, startPath) != 0))
       continue;
@@ -152,21 +152,29 @@ void recursive_travese(char *curPath) {
       fprintf(stderr, "Can't retrieve stat for %s: %s\n", tmp, strerror(errno));
       continue;
     }
-    if (userFlag && !check_user(&statbuf)) continue;
-    if ((mtimeFlag && check_mtime(statbuf.st_mtime))) continue;
+    if (userFlag && !check_user(&statbuf))
+      filtered = true;
+    if (filtered || (mtimeFlag && check_mtime(statbuf.st_mtime)))
+      filtered = true;
 
-    if ((pwd = getpwuid(statbuf.st_uid)) == NULL)userNotFound = true;
-    if ((grp = getgrgid(statbuf.st_gid)) == NULL) groupNotFound = true;
+    if ((pwd = getpwuid(statbuf.st_uid)) == NULL)
+      userNotFound = true;
+    if ((grp = getgrgid(statbuf.st_gid)) == NULL)
+      groupNotFound = true;
 
-    parse_info(direntp, &statbuf, pwd, grp, tmp, userNotFound, groupNotFound);
+    if (!filtered)
+      parse_info(direntp, &statbuf, pwd, grp, tmp, userNotFound, groupNotFound);
 
     if (direntp->d_type == DT_DIR && strcmp(direntp->d_name, ".") != 0) {
-      if (!volumeFlag) recursive_travese(tmp);
-      else if (devNum == statbuf.st_dev) recursive_travese(tmp);
-      else fprintf(stderr, "note: not crossing mount point at %s\n", tmp);
+      if (!volumeFlag || (volumeFlag && devNum == statbuf.st_dev))
+        recursive_traverse(tmp);
+      else
+        fprintf(stderr, "note: not crossing mount point at %s\n", tmp);
     }
   }
-  if (closedir(dirp) < 0) fprintf(stderr, "error closing directory %s: %s\n", curPath, strerror(errno));
+  if (closedir(dirp) < 0)
+    fprintf(stderr, "error closing directory %s: %s\n", curPath,
+            strerror(errno));
 }
 
 void parse_info(struct dirent *direntp, struct stat *statbuf,
@@ -176,10 +184,14 @@ void parse_info(struct dirent *direntp, struct stat *statbuf,
   print_block_num(statbuf->st_blocks / 2 + statbuf->st_blocks % 2);
   print_permission(statbuf->st_mode);
   print_nlinks(statbuf->st_nlink);
-  if (userNotFound) print_id(statbuf->st_uid);
-  else print_name(pwd->pw_name);
-  if (groupNotFound) print_id(statbuf->st_gid);
-  else print_name(grp->gr_name);
+  if (userNotFound)
+    print_id(statbuf->st_uid);
+  else
+    print_name(pwd->pw_name);
+  if (groupNotFound)
+    print_id(statbuf->st_gid);
+  else
+    print_name(grp->gr_name);
   print_size(statbuf);
   print_mtime(&statbuf->st_mtime);
   print_fname(tmp, statbuf->st_mode);
@@ -203,16 +215,17 @@ bool check_user(struct stat *statbuf) {
     }
   }
 
-  if (!curUserNameNotFound && sameGroup && statbuf->st_mode & S_IRGRP) return true;
-  if (!sameGroup && statbuf->st_mode & S_IROTH) return true;
+  if (!curUserNameNotFound && sameGroup && statbuf->st_mode & S_IRGRP)
+    return true;
+  if (!sameGroup && statbuf->st_mode & S_IROTH)
+    return true;
   return false;
 }
 
-bool check_uid_or_user(char *curUser) { return isdigit(*curUser); }
+bool check_uid_or_name(char *curUser) { return isdigit(*curUser); }
 
 bool check_mtime(__time_t mtime) {
   double timeDiff = difftime(currentTime, mtime);
-  // printf("%f %f\n", mtimeLimit, timeDiff);
   if (mtimeLimit >= 0) {
     if (timeDiff >= mtimeLimit)
       return false;
@@ -223,9 +236,9 @@ bool check_mtime(__time_t mtime) {
   return true;
 }
 
-void print_inode(__ino_t inode) { printf("%lu\t", inode); }
+void print_inode(__ino_t inode) { printf("%9lu ", inode); }
 
-void print_block_num(long block_num) { printf("%ld\t", block_num); }
+void print_block_num(long block_num) { printf("%7ld ", block_num); }
 
 // switch case inspiration from
 // https://stackoverflow.com/questions/10323060/printing-file-permissions-like-ls-l-using-stat2-in-c/44580683#44580683
@@ -268,25 +281,28 @@ void print_permission(__mode_t mode) {
   permission[7] = (mode & S_IROTH) ? 'r' : '-';
   permission[8] = (mode & S_IWOTH) ? 'w' : '-';
   permission[9] = (mode & S_IXOTH) ? 'x' : '-';
-  if (mode & S_ISUID) permission[3] = (mode & S_IXUSR) ? 's' : 'S';
-  if (mode & S_ISGID) permission[6] = (mode & S_IXGRP) ? 's' : 'S';
-  if (mode & S_ISVTX) permission[9] = (mode & S_IXOTH) ? 't' : 'T';
+  if (mode & S_ISUID)
+    permission[3] = (mode & S_IXUSR) ? 's' : 'S';
+  if (mode & S_ISGID)
+    permission[6] = (mode & S_IXGRP) ? 's' : 'S';
+  if (mode & S_ISVTX)
+    permission[9] = (mode & S_IXOTH) ? 't' : 'T';
   permission[10] = '\0';
-  printf("%s\t", permission);
+  printf("%10s ", permission);
 }
 
-void print_nlinks(__nlink_t nlinks) { printf("%ld\t", (long)nlinks); }
+void print_nlinks(__nlink_t nlinks) { printf("%3ld ", (long)nlinks); }
 
-void print_id(long id) { printf("%ld\t", id); }
+void print_id(long id) { printf("%3ld ", id); }
 
-void print_name(char *name) { printf("%s\t", name); }
+void print_name(char *name) { printf("%6s ", name); }
 
 void print_size(struct stat *statbuf) {
   if (S_ISBLK(statbuf->st_mode) || S_ISCHR(statbuf->st_mode)) {
-    printf("%ld, %ld\t", (long)major(statbuf->st_rdev),
+    printf("%3ld, %3ld ", (long)major(statbuf->st_rdev),
            (long)minor(statbuf->st_rdev));
   } else {
-    printf("%lld\t", (long long)statbuf->st_size);
+    printf("%8lld ", (long long)statbuf->st_size);
   }
 }
 
@@ -294,7 +310,7 @@ void print_mtime(__time_t *mtime) {
   struct tm *mt = localtime(mtime);
   char buf[4096];
   strftime(buf, sizeof(buf), "%b %d %R", mt);
-  printf("%s\t", buf);
+  printf("%s ", buf);
 }
 
 void print_fname(char tmp[], __mode_t mode) {
