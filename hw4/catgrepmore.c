cfb0da18d09126b92e4b1490a47e0bc973ce5fcb
Do Hyung Kwon <thedavekwon@gmail.com>
2019-11-06 13:45:40 -0500

    hw4

--- hw4/catgrepmore.c
+++ hw4/catgrepmore.c
@@ -29,80 +29,83 @@
 #define PCLOSE 16
 #define PWRITE 17
 #define WPID 18
+#define SIGACT 19
 
 #define BUF_SIZE 4096
 
 void check_error(int fd, int n, char *s, int type, int return_code);
 void run_grep(pid_t* grep_pid, char* cmd[]);
 void run_more(pid_t* more_pid, char* cmd[]);
-void int_handler();
-void cat_grep_more(char* filename);
+void int_handler(int sig);
+void cat_grep_more(char* pattern, char* filename);
 
-int cat_grep[2];
-int grep_more[2];
-jmp_buf int_jb;
+int cat_grep_pipe[2];
+int grep_more_pipe[2];
+sigjmp_buf jb;
 
-char* pattern;
+int totalFileCnt;
+long long totalBytes;
 
 int main(int argc, char* argv[]) {
     if (argc < 3) {
         fprintf(stderr, "Usage: catgrepmore pattern infile1 [...infile2...]\n");
         exit(EXIT_FAILURE);
     }
-    signal(SIGINT, int_handler);
-
-    int i;
-    pattern = malloc(sizeof(argv[i]));
-    if (pattern == NULL) {
-        fprintf(stderr, "Failed to allocate memory: %s\n", strerror(errno));
-        exit(EXIT_FAILURE);
+    struct sigaction sa;
+    sa.sa_handler = int_handler;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags=0;
+    check_error(sigaction(SIGINT, &sa, NULL), 0, "SIGINT", SIGACT, 0);
+    check_error(sigaction(SIGPIPE, &sa, NULL), 0, "SIGPIPE", SIGACT, 0);
+    int idx;
+    for (idx = 2; idx < argc; idx++) {
+        totalFileCnt++;
+        cat_grep_more(argv[1], argv[idx]);
     }
-    strcpy(pattern, argv[1]);
-    for (i = 2; i < argc; i++) {
-        cat_grep_more(argv[i]);
-        if (setjmp(int_jb) != 0) continue;
-    }
-    free(pattern);
 }
 
-void cat_grep_more(char* filename) {
+void cat_grep_more(char* pattern, char* filename) {
     int fd_r, bytes_read, bytes_written, total_bytes_written;
     char buf[BUF_SIZE];
-
+    bool skip = false;
     pid_t grep_pid, more_pid;
     int grep_status, more_status;
 
     char* grep_cmd[] = {"grep", pattern, NULL};
     char* more_cmd[] = {"more", NULL};
 
-    check_error(pipe(cat_grep), 0, "", PIPE, 0);
-    check_error(pipe(grep_more), 0, "", PIPE, 0);
+    check_error(pipe(cat_grep_pipe), 0, "GREP", PIPE, 0);
+    check_error(pipe(grep_more_pipe), 0, "MORE", PIPE, 0);
 
     run_grep(&grep_pid, grep_cmd);
     run_more(&more_pid, more_cmd);
 
-    check_error(close(cat_grep[0]), 0, "", PCLOSE, 0);
-    check_error(close(grep_more[0]), 0, "", PCLOSE, 0);
-    check_error(close(grep_more[1]), 0, "", PCLOSE, 0);
+    check_error(close(cat_grep_pipe[0]), 0, "GREP", PCLOSE, 0);
+    check_error(close(grep_more_pipe[0]), 0, "MORE", PCLOSE, 0);
+    check_error(close(grep_more_pipe[1]), 0, "MORE", PCLOSE, 0);
 
     // write to pipe here
     check_error((fd_r = open(filename, O_RDONLY)), 0, filename, ROPEN, 0);
     while ((bytes_read = read(fd_r, buf, BUF_SIZE)) != 0) {
+        if (sigsetjmp(jb, 1) != 0) break;
         check_error(fd_r, bytes_read, filename, READ, 0);
         bytes_written = 0, total_bytes_written = 0;
         while (bytes_written < bytes_read) {
-            bytes_written = write(cat_grep[1], buf + total_bytes_written, bytes_read - total_bytes_written);
-            check_error(cat_grep[1], bytes_written, "", PWRITE, 0);
+            bytes_written = write(cat_grep_pipe[1], buf + total_bytes_written, bytes_read);
+            check_error(cat_grep_pipe[1], bytes_written, "", PWRITE, 0);
+            bytes_read -= bytes_written;
             total_bytes_written += bytes_written;
         }
+        totalBytes += total_bytes_written;
     }
-    if (fd_r) check_error(fd_r, close(fd_r), filename, OCLOSE, 0);
 
-    check_error(close(cat_grep[1]), 0, "", PCLOSE, 0);
-    check_error(waitpid(grep_pid, &grep_status, 0), 0, "GREP", WPID, 0);
-    check_error(waitpid(more_pid, &more_status, 0), 0, "MORE", WPID, 0);
-    if (!WIFEXITED(grep_status)) fprintf(stderr, "GREP exited unexpectedly\n");
-    if (!WIFEXITED(more_status)) fprintf(stderr, "MORE exited unexpectedly\n");
+    if (!skip) {
+        if (fd_r) check_error(fd_r, close(fd_r), filename, OCLOSE, 0);
+        check_error(close(cat_grep_pipe[1]), 0, "GREP", PCLOSE, 0);
+        skip = true;
+        check_error(waitpid(grep_pid, &grep_status, 0), 0, "GREP", WPID, 0);
+        check_error(waitpid(more_pid, &more_status, 0), 0, "MORE", WPID, 0);
+    }
 }
 
 void run_grep(pid_t* grep_pid, char* cmd[]) {
@@ -112,16 +115,15 @@ void run_grep(pid_t* grep_pid, char* cmd[]) {
             perror("Failed to fork GREP");
             exit(EXIT_FAILURE);
         case 0:
-            check_error(dup2(cat_grep[0], STDIN_FILENO), 0, "", DUP, 0);
-            check_error(dup2(grep_more[1], STDOUT_FILENO), 0, "", DUP, 0);
-            check_error(close(cat_grep[0]), 0, "", PCLOSE, 0);
-            check_error(close(cat_grep[1]), 0, "", PCLOSE, 0);
-            check_error(close(grep_more[0]), 0, "", PCLOSE, 0);
-            check_error(close(grep_more[1]), 0, "", PCLOSE, 0);
+            check_error(dup2(cat_grep_pipe[0], STDIN_FILENO), 0, "", DUP, 0);
+            check_error(dup2(grep_more_pipe[1], STDOUT_FILENO), 0, "", DUP, 0);
+            check_error(close(cat_grep_pipe[0]), 0, "GREP", PCLOSE, 0);
+            check_error(close(cat_grep_pipe[1]), 0, "GREP", PCLOSE, 0);
+            check_error(close(grep_more_pipe[0]), 0, "MORE", PCLOSE, 0);
+            check_error(close(grep_more_pipe[1]), 0, "MORE", PCLOSE, 0);
             execvp(cmd[0], cmd);
             fprintf(stderr, "Failed to run grep execvp: %s\n", strerror(errno));
             exit(EXIT_FAILURE);
-            break;
         default:
             *grep_pid = pid;
             break;
@@ -135,23 +137,23 @@ void run_more(pid_t* more_pid, char* cmd[]) {
             perror("Failed to fork MORE");
             exit(EXIT_FAILURE);
         case 0:
-            check_error(dup2(grep_more[0], STDIN_FILENO), 0, "", DUP, 0);
-            check_error(close(cat_grep[0]), 0, "", PCLOSE, 0);
-            check_error(close(cat_grep[1]), 0, "", PCLOSE, 0);
-            check_error(close(grep_more[0]), 0, "", PCLOSE, 0);
-            check_error(close(grep_more[1]), 0, "", PCLOSE, 0);
+            check_error(dup2(grep_more_pipe[0], STDIN_FILENO), 0, "", DUP, 0);
+            check_error(close(cat_grep_pipe[0]), 0, "GREP", PCLOSE, 0);
+            check_error(close(cat_grep_pipe[1]), 0, "GREP", PCLOSE, 0);
+            check_error(close(grep_more_pipe[0]), 0, "MORE", PCLOSE, 0);
+            check_error(close(grep_more_pipe[1]), 0, "MORE", PCLOSE, 0);
             execvp(cmd[0], cmd);
             fprintf(stderr, "Failed to run more execvp: %s\n", strerror(errno));
             exit(EXIT_FAILURE);
-            break;
         default:
             *more_pid = pid;
             break;
     }
 }
 
-void int_handler() {
-    longjmp(int_jb, 1);
+void int_handler(int sig) {
+    if (sig == SIGINT) fprintf(stderr, "%d files and %lld bytes processed.\n", totalFileCnt, totalBytes);
+    siglongjmp(jb, 1);
 }
 
 void check_error(int fd, int n, char *s, int type, int return_code) {
@@ -194,16 +196,19 @@ void check_error(int fd, int n, char *s, int type, int return_code) {
                 fprintf(stderr, "Failed to getline: %s\n", strerror(errno));
                 break;
             case PIPE:
-                fprintf(stderr, "Failed to create pipe: %s\n", strerror(errno));
+                fprintf(stderr, "Failed to create %s pipe: %s\n", s, strerror(errno));
                 break;
             case PCLOSE:
-                fprintf(stderr, "Can't close pipe: %s\n", strerror(errno));
+                fprintf(stderr, "Can't close %s pipe: %s\n", s, strerror(errno));
                 break;
             case PWRITE:
                 fprintf(stderr, "Can't write to pipe: %s\n", strerror(errno));
                 break;
             case WPID:
                 fprintf(stderr, "Failed to wait %s pid: %s\n", s, strerror(errno));
+                break;
+            case SIGACT:
+                fprintf(stderr, "Failed to attach handler to signal %s: %s\n", s, strerror(errno));
                 break;
             default:
                 break;
